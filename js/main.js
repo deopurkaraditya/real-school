8c72b75417bf28ae14e17fb90b84d20aa1e2a8a6
Aditya Deopurkar <deopurkaraditya@gmail.com>
2020-12-09 13:28:07 +0530

    Slider changes

--- js/main.js
+++ js/main.js
@@ -1,37 +1,6 @@
-var slideIndex = 1
-showSlides(slideIndex)
-
-function plusSlides(n) {
-  showSlides((slideIndex += n))
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n))
-}
-
-function showSlides(n) {
-  var i
-  var slides = document.getElementsByClassName('slides')
-  var dots = document.getElementsByClassName('dot')
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none'
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '')
-  }
-  slides[slideIndex - 1].style.display = 'block'
-  dots[slideIndex - 1].className += ' active'
-}
-
 function openNav() {
   if (screen.width < 700) {
-    document.getElementById('realSidepanel').style.width = '100%'
+    document.getElementById('6').style.width = '100%'
   } else {
     document.getElementById('realSidepanel').style.width = '30%'
   }
@@ -47,5 +16,82 @@ function toggleIcon(e) {
     .find('.more-less')
     .toggleClass('fa-angle-right fa-angle-down')
 }
-$('.accordion').on('hidden.bs.collapse', toggleIcon)
-$('.accordion').on('shown.bs.collapse', toggleIcon)
+
+$('.skill-slider').slick({
+  lazyLoad: 'ondemand',
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 980, // tablet breakpoint
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 480, // mobile breakpoint
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+      },
+    },
+  ],
+})
+
+$(document).ready(function () {
+  if ($('.creative-thinking').hasClass('slick-current')) {
+    $('.slick-prev').hide()
+  }
+})
+
+$(document).on('click', 'body *', function () {
+  if ($('.creative-thinking').hasClass('slick-current')) {
+    $('.slick-prev').hide()
+  } else {
+    $('.slick-prev').show()
+  }
+})
+
+// $('.badges-slider-mobile').slick({
+//   responsive: [
+//     {
+//       lazyLoad: 'ondemand',
+//       breakpoint: 480, // mobile breakpoint
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         arrows: false,
+//         dots: true,
+//       },
+//     },
+//   ],
+// })
+
+if (window.matchMedia('(max-width: 768px)').matches) {
+  /* the viewport is less than 768 pixels wide */
+  $('.badges-slider-mobile').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+  })
+
+  $('.card-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+  })
+  $('.card-deck-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+  })
+}
